7101b503b712ce40e4a6ac2f27b2f9ab96c757be
Shane Chintatkun <schintatkun@gmail.com>
2018-11-19 19:40:57 -0800

    finished reverse String

--- reverse_string.js
+++ reverse_string.js
@@ -1,4 +1,8 @@
 // start algorithm projects
 function reverseString(str){
-    
+    var newStr = '';
+    for (let i=str.length-1;i<=0;i--){
+        newStr += str[i];
+    }
+    return newStr;
 }
