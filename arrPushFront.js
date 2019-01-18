0495501e20a72bc784098657645c2342c96f7673
Shane Chintatkun <schintatkun@gmail.com>
2019-01-17 23:33:42 -0800

    fixed error on arrPushFront

--- arrPushFront.js
+++ arrPushFront.js
@@ -1,8 +1,9 @@
 function arrPushFront(arr, insertValue){
     for (var i=arr.length; i>0;i--){
-        arr.length = arr.length-1;
+        arr[i] = arr[i-1];
     }
     arr[0]= insertValue;
     return arr;
 }
-arrPushFront([1,2,3,4,5], 77);
+console.log(`Before : [1,2,3,4,5]`);
+console.log(arrPushFront([1,2,3,4,5], 77));
