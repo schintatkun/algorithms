93ecbf07345aaf31ed8d1a3f2e0f329eb4b82263
Shane Chintatkun <schintatkun@gmail.com>
2019-02-20 23:59:08 -0800

    added recursive find uppercase letter

--- Python/findUppercaseLetter.py
+++ Python/findUppercaseLetter.py
@@ -1,4 +1,5 @@
 inputStr = "seattleWashington"
+inputStr2 = "lovecoding"
 
 # iterative method
 def find_uppercase_iterative(inputStr):
@@ -8,3 +9,15 @@ def find_uppercase_iterative(inputStr):
     return "No uppercase letter found"
 
 print(find_uppercase_iterative(inputStr))
+
+def find_uppercase_recursive(inputStr, idx=0):
+    # base cases
+    if inputStr[idx].isupper():
+        return inputStr[idx]
+    
+    # process to the end of string
+    if idx == len(inputStr)-1:
+        return "No uppercase letter found"
+    return find_uppercase_recursive(inputStr, idx+1)
+
+print(find_uppercase_recursive(inputStr2))
