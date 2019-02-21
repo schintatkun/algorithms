18bef906cf2ae410cd50068d1346575b344f9395
Shane Chintatkun <schintatkun@gmail.com>
2019-02-21 02:19:11 -0800

    add comment on recursive base cases, and fixed error

--- Python/findUppercaseLetter.py
+++ Python/findUppercaseLetter.py
@@ -11,11 +11,13 @@ def find_uppercase_iterative(inputStr):
 print(find_uppercase_iterative(inputStr))
 
 def find_uppercase_recursive(inputStr, idx=0):
-    # base cases
+    # 2 base cases
+    # idx we are at is an uppercase, then return
     if inputStr[idx].isupper():
         return inputStr[idx]
     
-    # process to the end of string
+    # process to the end of string, current position is at last idx.
+    # which mean no uppercase found.
     if idx == len(inputStr)-1:
         return "No uppercase letter found"
     return find_uppercase_recursive(inputStr, idx+1)
