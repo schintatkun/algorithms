0c4d1fa5f4222d23df6c0615cb6d19d62660819c
Shane Chintatkun <schintatkun@gmail.com>
2019-02-25 22:01:57 -0800

    added condition to palindrome algoritmn

--- Python/palindrome.py
+++ Python/palindrome.py
@@ -5,6 +5,10 @@ def is_palindrome(str):
     j = len(str)-1
 
     while i < j:
+        while not str[i].isalnum() and i < j:
+            i += 1
+        while not str[i].isalnum() and i < j:
+            j += 1
         if str[i] != str[j]:
             return False
         i+= 1
@@ -12,3 +16,4 @@ def is_palindrome(str):
     return True
 
 print(is_palindrome("racabe"))
+print(is_palindrome("racecar"))
