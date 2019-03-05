bbebb803cc39ba21ac559bf6541c7b14d02cd351
Shane Chintatkun <schintatkun@gmail.com>
2019-03-04 23:50:20 -0800

    added 3rd apporch to unique stirng function

--- Python/unique_str.py
+++ Python/unique_str.py
@@ -17,6 +17,16 @@ def is_unique(inputStr):
 def is_unique2(inputStr):
     return len(set(inputStr))== len(inputStr)
 
+def is_unique3(inputStr):
+    alpha = "abcdefghigklmnopqrstuvwxyz"
+
+    for i in inputStr:
+        if i in alpha:
+            alpha = alpha.replace(i,"")
+        else:
+            return False
+    return True
+
 #normalize input string
 
 str1 = normalize(str1)
@@ -27,3 +37,6 @@ print(is_unique(str2))
 
 print(is_unique2(str1))
 print(is_unique2(str2))
+
+print(is_unique3(str1))
+print(is_unique3(str2))
