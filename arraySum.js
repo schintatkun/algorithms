365967febb22d041930ad194044b544e81cd32e3
Shane Chintatkun <schintatkun@gmail.com>
2020-01-01 22:45:12 -0800

    edited arraySum.js

--- arraySum.js
+++ arraySum.js
@@ -1,5 +1,6 @@
-function arraySum(arr){
-    return arr.reduce((a,b) => a+b);
+function arraySum(arr) {
+  return arr.reduce((a, b) => a + b);
 }
 
-console.log(arraySum([1,2,3,4,5]));
+console.log(arraySum([1, 2, 3, 4, 5]));
+console.log(arraySum([2, 4, 6, 8, 10, 12]));
