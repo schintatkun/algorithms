04927eb1c59f4caa65b73dd725cfd885f7bb4f5a
Shane Chintatkun <schintatkun@gmail.com>
2020-01-15 00:11:41 -0800

    added find(data) in binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -56,4 +56,19 @@ class BST {
     }
     return current.data;
   }
+  //find a value
+  find(data) {
+    let current = this.root;
+    while (current.data !== data) {
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+      if (current === null) {
+        return null;
+      }
+    }
+    return current;
+  }
 }
