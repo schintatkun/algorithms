95e44099f6abf246d2143429eed6b6140b6e49c3
Shane Chintatkun <schintatkun@gmail.com>
2020-01-22 23:42:31 -0800

    added comment in an initial code hashTable.js

--- hashTable.js
+++ hashTable.js
@@ -1,9 +1,12 @@
 // Hash Table
 
+// "string" that we want to hash
+// "max" parameter is number of buckets that's using in the hash table to store values
 var hash = (string, max) => {
   var hash = 0;
   for (var i = 0; i < string.length; i++) {
     hash += string.charCodeAt(i);
   }
+  //return the remainder
   return hash % max;
 };
