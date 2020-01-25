76305b737b08bbc2068f19dc9785c0f50ce86e8a
Shane Chintatkun <schintatkun@gmail.com>
2020-01-24 23:20:22 -0800

    added add function in hashTable.js

--- hashTable.js
+++ hashTable.js
@@ -7,6 +7,43 @@ var hash = (string, max) => {
   for (var i = 0; i < string.length; i++) {
     hash += string.charCodeAt(i);
   }
-  //return the remainder
+  //return the remainder, which is an index in array
   return hash % max;
+};
+
+let HashTable = function() {
+  let storage = [];
+  const storageLimit = 4;
+
+  // print all items in storage array
+  this.print = function() {
+    console.log(storage);
+  };
+
+  // add item function
+  this.add = function(key, value) {
+    // find an index of array by running through hash function
+    var index = hash(key, storageLimit);
+
+    // if nothing in this index of storage array, then set key && value pairs
+    // to this index
+    if (storage[index] === undefined) {
+      storage[index] = [[key, value]];
+    } else {
+      var inserted = false;
+
+      // go through all index if the key already exists.
+      for (var i = 0; i < storage[index].length; i++) {
+        // if key already exists, then set a new value here.
+        if (storage[index][i][0] === key) {
+          storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      // if the key does not exist, then push in key & value into storage array
+      if (inserted === false) {
+        storage[index].push([key, value]);
+      }
+    }
+  };
 };
