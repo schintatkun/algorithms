d1988980770c3febe814acec2078ea5f240fb04c
Shane Chintatkun <schintatkun@gmail.com>
2020-08-20 00:02:51 -0700

    fixed an error in insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -2,15 +2,15 @@ function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let current = arr[i];
     let j = i - 1;
-    while (j >= 0 && arr[i] > current) {
+    while (j >= 0 && arr[j] > current) {
       arr[j + 1] = arr[j];
       j--;
     }
     arr[j + 1] = current;
   }
-  console.log(arr);
+  //   console.log(arr);
   return arr;
 }
-console.log("Sort Empty Array : ", insertionSort([]));
-console.log("Sort array that has a single value: ", insertionSort([2]));
-console.log(insertionSort([2, 10, 22, 5, 3, 0, 102, 76]));
+// console.log("Sort Empty Array : ", insertionSort([]));
+// console.log("Sort array that has a single value: ", insertionSort([2]));
+console.log(insertionSort([2, 10, 5, 22, 5, 3, 0, 1, 102, 1, 76]));
